c7c190941676cce5118f16897044ffc108b27801
ManojSankolSR <manojsankol6361@gmail.com>
2025-04-23 15:39:42 +0530

    fix: Correct export statements in index.ts to ensure proper module exports

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,8 @@
+// Named Exports
 export * from "./SCORM/view/components/ScormPlayer";
+export * from "./SCORM/models/API";
+export * from "./SCORM/models/SCORMDataModel";
 
-export * from "../src/SCORM/models/API";
-export * from "../src/SCORM/models/SCORMDataModel";
+// Default Export (only one default allowed)
+import ScormPlayer from "./SCORM/view/components/ScormPlayer";
+export default ScormPlayer;
