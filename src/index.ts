4b48accc4bf1f0e41228b89ee20c4728cc04ade7
ManojSankolSR <manojsankol6361@gmail.com>
2025-04-23 15:34:13 +0530

    fix: Update export statement for ScormPlayerProps to use wildcard export

--- src/index.ts
+++ src/index.ts
@@ -1,4 +1,4 @@
-export { default as ScormPlayerProps } from "../src/SCORM/view/components/ScormPlayer";
+export * from "./SCORM/view/components/ScormPlayer";
 
 export * from "../src/SCORM/models/API";
 export * from "../src/SCORM/models/SCORMDataModel";
